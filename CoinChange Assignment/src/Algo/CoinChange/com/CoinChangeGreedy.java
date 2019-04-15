5210c1115a977e7ed912806b99527a62d303544b
rahulratra <rahulratra@192.168.1.27>
2019-04-14 20:31:59 -0400

    Algo Assignment Dynamic Approqach

---
+++ CoinChange Assignment/src/Algo/CoinChange/com/CoinChangeGreedy.java
@@ -0,0 +1,35 @@
+package Algo.CoinChange.com;
+
+import java.util.ArrayList;
+
+public class CoinChangeGreedy {
+
+    public static void findMin(int amount)
+    {
+        int coins[] = { 1, 5, 10, 25 };
+        //int[] coinList = {};
+        int[] arr = new int[100];
+        int i, k = 0;
+        for (i = coins.length - 1; i >= 0; i--) {
+            while (amount >= coins[i]) {
+                amount -= coins[i];
+                int max_coins = coins[i];
+                //System.out.println(" coins"+ coins[i]);
+                arr[k++] = max_coins;
+            }
+        }
+        System.out.println("Denominations of coins :"  );
+
+        for (i = 0; i < k; i++) {
+            System.out.println(arr[i]);
+        }
+        System.out.println("Minimum_Number of Coins using GreedyAlgorithm :"+ k);
+        return;
+    }
+
+    public static void main (String[] args)
+    {
+        int n = 200;
+        findMin(n);
+    }
+}
