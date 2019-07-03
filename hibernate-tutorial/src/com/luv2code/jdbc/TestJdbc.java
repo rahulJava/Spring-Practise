c25c745e4d4b3fc6cc4fa17df8f8efc9a773ad5d
rahulratra <rahulratra@192.168.1.8>
2019-07-03 12:53:33 -0400

    Test

--- hibernate-tutorial/src/com/luv2code/jdbc/TestJdbc.java
+++ hibernate-tutorial/src/com/luv2code/jdbc/TestJdbc.java
@@ -1,5 +1,18 @@
 package com.luv2code.jdbc;
 
-public class TestJdbc {
-
+public class TestJdbc
+{
+public static void main(String[] args) {
+	
+	String jdbcurl= "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+	String user="hbstudent";
+	String pass="hbstudent";
+	try
+	{
+	
+	}
+	catch (Exception e) {
+		// TODO: handle exception
+	}
+}
 }
