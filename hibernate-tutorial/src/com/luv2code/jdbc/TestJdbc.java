f330fe568858e6d88f7568ac4bb4ec7bac420345
rahulratra <rahulratra@192.168.1.8>
2019-07-03 13:05:54 -0400

    Commit

--- hibernate-tutorial/src/com/luv2code/jdbc/TestJdbc.java
+++ hibernate-tutorial/src/com/luv2code/jdbc/TestJdbc.java
@@ -1,17 +1,24 @@
 package com.luv2code.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class TestJdbc
 {
 public static void main(String[] args) {
 	
 	String jdbcurl= "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
 	String user="hbstudent";
-	String pass="hbstudent";
+	String pass="hbstudent1";
 	try
 	{
 	
+		System.out.println("connect to database");
+		Connection myconn= DriverManager.getConnection(jdbcurl,user,pass);
+		System.out.println("Connection Successful");
 	}
 	catch (Exception e) {
+		e.printStackTrace();
 		// TODO: handle exception
 	}
 }
