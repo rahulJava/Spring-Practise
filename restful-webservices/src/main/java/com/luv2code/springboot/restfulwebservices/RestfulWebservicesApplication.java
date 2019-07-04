53478bfde796d59ef074ec1d8b4ced24e0895e7a
rahulratra <rahulratra@192.168.1.8>
2019-07-04 16:31:03 -0400

    <commit>

--- restful-webservices/src/main/java/com/luv2code/springboot/restfulwebservices/RestfulWebservicesApplication.java
+++ restful-webservices/src/main/java/com/luv2code/springboot/restfulwebservices/RestfulWebservicesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestfulWebservicesApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hi");
+		System.out.println("Hi1");
 		SpringApplication.run(RestfulWebservicesApplication.class, args);
 	}
 
